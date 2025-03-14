0c089ae2c9271c5213533641760fcb7c2acdd2e3
Fernando Paredes Rios <fprios112@gmail.com>
2025-03-14 01:34:40 -0500

    shell for superuser temporal

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@
 python manage.py makemigrations
 python manage.py migrate
 
+# Crear superusuario (no recomendado)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'fprios112@gmail.com', 'Fer123456*')" | python manage.py shell
+
 # Iniciar el servidor
 python manage.py runserver 0.0.0.0:8000
